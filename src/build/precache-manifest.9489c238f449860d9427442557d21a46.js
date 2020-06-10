32d4ec6aa867d8781a4ea4d42cfa2f11aa249daf
CyberFXL <imanh947@gmail.com>
2020-06-10 10:18:50 +0800

    First commit

---
+++ src/build/precache-manifest.9489c238f449860d9427442557d21a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5f8297f9391b36326ef1ef9afd9ee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b5e84ede30afc75b9bd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19d68f4f5b08ec286592",
+    "url": "/static/js/2.ca9b9c86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca9b9c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5e84ede30afc75b9bd",
+    "url": "/static/js/main.c1fd6c2f.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
